3627ae9f255f83dd88be3192e7e2193804fd53c8
Simon <SimonNDao12@gmail.com>
2021-03-02 22:59:30 -0800

    fixed bug with new.esj giving error when rendered

--- routes/articles.js
+++ routes/articles.js
@@ -3,15 +3,16 @@ const Article = require('./../models/article')
 const router = express.Router()
 
 router.get('/new', (req, res) => {
-    res.render('articles/new')
+
+    res.render('../views/articles/new',{article: new Article()})
 })
 
 router.get('/:id', (req,res) => {
-
+    
 })
 
 router.post('/', async (req, res) => {
-    /*const article = new Article({
+    const article = new Article({
         title: req.body.title,
         description: req.body.description,
         markdown: req.body.markdown,
@@ -20,10 +21,8 @@ router.post('/', async (req, res) => {
         article = await article.save()
         res.redirect(`/articles/${article.id}`)
     } catch (e) {
-        res.render('articles/new',{ article: article })
+        res.render('../views/articles/new',{ article: article })
     }
-    */
-   console.log(req.body.title)
 }) 
 
 module.exports = router
