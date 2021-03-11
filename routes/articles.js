d4b8283a4a4694e93fa54280c5981f0ec85d2752
Simon <SimonNDao12@gmail.com>
2021-03-10 23:09:45 -0800

    program can now edit articles yay

--- routes/articles.js
+++ routes/articles.js
@@ -2,6 +2,11 @@ const express = require('express')
 const Article = require('./../models/article')
 const router = express.Router()
 
+router.get('/edit/:id', async (req,res) => {
+    const article = await Article.findById(req.params.id)
+    res.render('../views/articles/edit',{article: article})
+})
+
 router.get('/new', (req, res) => {
     res.render('../views/articles/new',{article: new Article()})
 })
@@ -17,19 +22,31 @@ router.get('/:slug', async (req,res) => {
     res.render('articles/show',{article: article})
 })
 
-router.post('/', async (req, res) => {
-    let article = new Article({
-        title: req.body.title,
-        description: req.body.description,
-        markdown: req.body.markdown,
-    })
-    try {
-        article = await article.save()
-        res.redirect(`/articles/${article.slug}`)
-    } catch (e) {
-        console.log(e)
-        res.render('../views/articles/new',{ article: article })
+router.put('/:id', async (req,res, next)=> {
+    req.article = await Article.findById(req.params.id)
+    next()
+}, saveArticleAndRedirect('edit'))
+
+router.post('/', async (req, res, next) => {
+    req.article = new Article()
+    next()
+}, saveArticleAndRedirect('new')) 
+
+function saveArticleAndRedirect(path) {
+    return async (req,res) => {
+
+        let article = req.article 
+            article.title = req.body.title
+            article.description = req.body.description
+            article.markdown = req.body.markdown
+        try {
+            article = await article.save()
+            res.redirect(`/articles/${article.slug}`)
+        } catch (e) {
+            console.log(e)
+            res.render(`../views/articles/${path}`,{ article: article })
+        }
     }
-}) 
+}
 
 module.exports = router
